e1110d6022a2b342e6555311077ba937ae8a2e45
garret.meier <garret.meier@bunchball.com>
2015-03-17 23:41:52 -0500

    started login screen

--- src/main/java/garret/opossum/OpossumApplication.java
+++ src/main/java/garret/opossum/OpossumApplication.java
@@ -1,6 +1,12 @@
 package garret.opossum;
 
+import java.util.EnumSet;
 import java.util.Map;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -44,6 +50,18 @@ public class OpossumApplication extends Application<OpossumConfiguration> {
     @Override
     public void run(OpossumConfiguration configuration,
                     Environment environment) {
+    	
+    	final FilterRegistration.Dynamic cors =
+    	        environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+    	    // Configure CORS parameters
+    	    cors.setInitParameter("allowedOrigins", "*");
+    	    cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+    	    cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+    	    // Add URL mapping
+    	    cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
         environment.jersey().register(new SearchResource());
         environment.jersey().register(new UserResource());
         
